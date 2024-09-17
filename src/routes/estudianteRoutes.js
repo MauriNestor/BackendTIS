2671517ee63c656fe2a728fbb72972e084a16403
MichelleBarriga <barriga.rios.michelle1@gmail.com>
2024-09-16 22:55:13 -0400

    cambiar contraseña

--- src/routes/estudianteRoutes.js
+++ src/routes/estudianteRoutes.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 const estudianteController = require('../controllers/estudianteController');
 const validarEstudiante = require('../middlewares/validarEstudiante');
+const validarPassword = require('../middlewares/validarPassword');
 
 router.post('/registro', validarEstudiante, estudianteController.registrarEstudiante);
 router.get('/', estudianteController.obtenerEstudiantes);
+router.post('/cambiar-password', validarPassword, estudianteController.setPassword);
 
 module.exports = router;

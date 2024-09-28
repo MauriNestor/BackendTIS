4cbc1bf75bfc4bf194d58ca3c4cc3c29fc5956d1
MichelleBarriga <barriga.rios.michelle1@gmail.com>
2024-09-27 22:36:33 -0400

    registrar requerimientos

--- src/services/planificacionService.js
+++ src/services/planificacionService.js
@@ -1,5 +1,6 @@
 const db = require('../config/db');
 const pool = db.pool; 
+
 const registrarPlanificacion = async (codigoClase, codigoGrupo) => {
     try {
         const codigoDocente = await getDocente(codigoClase); 
@@ -10,22 +11,43 @@ const registrarPlanificacion = async (codigoClase, codigoGrupo) => {
         const codProduct = result.rows[0].cod_product;
        
     } catch (err) {
-        console.error('Error al crear actividad', err);
+        console.error('Error al crear product backlog', err);
         throw err;
     }
 };
 
-const registrarRequerimientos = async (codigoClase, codigoGrupo) => {
+const registrarRequerimientos = async (codigoProduct, requerimientos) => {
     try {
-        const codigoDocente = await getDocente(codigoClase); 
-        const result = await pool.query(
-            'INSERT INTO productbacklog (cod_docente, cod_clase, cod_grupoempresa) VALUES ($1, $2, $3) RETURNING *',
-            [codigoDocente, codigoClase, codigoGrupo]
-        );
-        const codProduct = result.rows[0].cod_product;
+        const estado = "Pendiente";
        
+        for (const requerimiento of requerimientos) {
+            
+            const result = await pool.query(
+                'INSERT INTO requerimiento (cod_product, requerimiento, decripcion_hu, prioridad_hu, estimacion_hu, estado_hu) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *',
+                [codigoProduct, requerimiento.requerimiento, requerimiento.descripcion, requerimiento.prioridad, requerimiento.estimacion, estado] 
+            ); 
+        }
     } catch (err) {
-        console.error('Error al crear actividad', err);
+        console.error('Error al crear requerimiento', err);
+        throw err;
+    }
+};
+
+const registrarSprint = async (sprint, fechaInicio, fechaFin, objetivo, requerimientos) => {
+    try {
+        const result = await pool.query(
+            'INSERT INTO sprint (sprint, fecha_inicio_sprint, fecha_fin_sprint, objetivo_sprint) VALUES ($1, $2, $3, $4) RETURNING *',
+            [sprint, fechaInicio, fechaFin, objetivo]
+        );
+        const codSprint = result.rows[0].cod_sprint;
+        for (const requerimiento of requerimientos) {
+            const result = await pool.query(
+                'UPDATE requermiento SET cod_sprint = $1 WHERE cod_requerimiento = $2',
+                [codSprint, requerimiento.codRequermiento]
+            );
+        }
+    } catch (err) {
+        console.error('Error al registrar sprintbacklog', err);
         throw err;
     }
 };
@@ -40,4 +62,6 @@ const getDocente = async (codigoClase) => {
 
 module.exports = {
     registrarPlanificacion, 
+    registrarRequerimientos,
+    registrarSprint,
 };

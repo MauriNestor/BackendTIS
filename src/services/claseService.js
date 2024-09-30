87cbaae098446abcef3142c73d9bcc6e3f89955e
MauriNestor <mauric845apaza@gmail.com>
2024-09-30 16:39:27 -0400

    add consulta de clases de un docente

--- src/services/claseService.js
+++ src/services/claseService.js
@@ -12,7 +12,20 @@ const ClaseService = {
       console.error('Error en la consulta de creación de clase:', error);
       throw new Error('Error al crear la clase en la base de datos');
     }
+  },
+  obtenerClasesPorDocente: async (codDocente) => {
+    try {
+      const result = await pool.query(
+        'SELECT cod_clase, nombre_clase, cod_gestion FROM clase WHERE cod_docente = $1',
+        [codDocente]
+      );
+      return result.rows;
+    } catch (error) {
+      console.error('Error en la consulta de obtención de clases por docente:', error);
+      throw new Error('Error al obtener las clases del docente en la base de datos');
+    }
   }
 };
+
 
 module.exports = ClaseService;

73b6d0541cdb69edae93b6e89c69f0b4c780fca8
MauriNestor <mauric845apaza@gmail.com>
2024-10-02 19:08:25 -0400

    remove: validaciones con gestion(solo acepta 1 y 2)

--- src/schemas/claseSchema.js
+++ src/schemas/claseSchema.js
@@ -2,12 +2,11 @@ const { z } = require('zod');
 
 const claseSchema = z.object({
   codGestion: z.number()
-    .int('El código de gestión debe ser un número entero')
-    .min(2024, 'El código de gestión debe ser mayor a 2023') 
-    .max(2026, 'El código de gestión no debe ser superior a 9999')
-    .refine(value => value !== null && value !== undefined, { message: 'El año de gestión es obligatorio' }),
-  
-    nombreClase: z.string()
+    .int()
+    .refine(val => val === 1 || val === 2, {
+      message: 'El código de gestión debe ser 1 o 2'
+    }), // Permitir solo 1 o 2
+  nombreClase: z.string()
     .min(1, 'El nombre de la clase es obligatorio')
     .max(50, 'El nombre de la clase no debe exceder 50 caracteres')
     .regex(/^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑ\s.,;:()\-]+$/, 'El nombre de la clase solo debe contener letras, números, espacios y caracteres especiales permitidos')

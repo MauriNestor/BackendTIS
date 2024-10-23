23eadbbf4fc615fe8f9a4a9823fd9c1c6eff818d
MichelleBarriga <barriga.rios.michelle1@gmail.com>
2024-10-22 22:46:07 -0400

    registrar horario setvice

--- src/services/horarioService.js
+++ src/services/horarioService.js
@@ -1,4 +1,5 @@
 const { pool } = require('../config/db');
+const planificacionService = require('../services/planificacionService');
 
 const getHorario = async (codigoHorario) => {
     try {
@@ -30,7 +31,25 @@ const getCodHorario = async (codigoGrupo) => {
     }
 };
 
+const registrarHorario = async (codClase, horario) => {
+    try {
+        const codDocente = await planificacionService.getDocente(codClase);
+        const result = await pool.query(
+        `INSERT INTO Horario (cod_docente, cod_clase, dia_horario, hora_inicio, hora_fin)
+	    VALUES ($1, $2, $3, $4, $5) RETURNING *`,
+            [codDocente, codClase, horario.diaHorario, horario.horaInicio, horaFin]
+        );
+        const codHorario = result.rows[0].cod_horario;
+        return codHorario;
+
+    }  catch (err) {
+        console.error('Error al registrar el horario', err);
+        throw err;
+    }
+};
+
 module.exports = {
     getHorario,
     getCodHorario,  // Verifica que esté correctamente exportada
+    registrarHorario,
 };

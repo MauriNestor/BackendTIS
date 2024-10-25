47d5200b12bc99d095d2e5aaecf8223307f81def
MichelleBarriga <barriga.rios.michelle1@gmail.com>
2024-10-24 20:36:14 -0400

    sin usar puerto

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-const app = require('./src/app'); 
-const PORT = process.env.PORT || 3000;
+const app = require('./src/app');
 
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en: http://localhost:${PORT}`);
-});
+module.exports = app;

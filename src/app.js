c08355447ad98037998bd55b5380b4769a68175c
MauriNestor <mauric845apaza@gmail.com>
2024-10-30 00:27:53 -0400

    Revert "Merge pull request #39 from MauriNestor/bug_captcha"
    This reverts commit 10abb2cb98a8db029a556878279e1363653cc31c, reversing
    changes made to 4f17861d307b0ce20d88e38d1b74e17e99ebcdd0.

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
-
 const docenteRoutes = require("./routes/docenteRoutes");
 const estudianteRoutes = require("./routes/estudianteRoutes");
 const pruebaRoutes = require("./routes/healthy");
@@ -16,7 +15,6 @@ const registroGrupoRoutes = require("./routes/registroGrupoRoutes");
 const evaluacionRoutes = require('./routes/evaluacionRoutes');
 const temaRoutes = require('./routes/temaRoute');
 const rubricaRoutes = require('./routes/rubricaRoutes');
-
 const db = require("./config/db");
 
 // Cargar las variables de entorno
@@ -52,7 +50,6 @@ app.use("/gestiones", gestionRoutes);
 app.use('/evaluaciones', evaluacionRoutes);
 app.use('/temas', temaRoutes);
 app.use('/rubricas', rubricaRoutes);
-
 // Manejo de rutas no encontradas
 app.use((req, res) => {
   res.status(404).json({ error: "Ruta no encontrada" });
--- src/services/loginService.js
+++ src/services/loginService.js
@@ -1,35 +1,13 @@
 const db = require('../config/db');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const axios = require('axios');
 
-// Función para generar el token JWT
 const generarToken = (payload) => {
     return jwt.sign(payload, process.env.SECRET_KEY, { expiresIn: '1h' }); // El token expirará en 1 hora
 };
 
-// Función para verificar el token de reCAPTCHA
-async function verifyCaptcha(captchaToken) {
-    const secretKey = "6LeFdWsqAAAAADuaUlGwF1N1toZ0LSv-7o1Y6cOR";  // Coloca aquí tu clave secreta
-    const url = `https://www.google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${captchaToken}`;
-
+const obtenerDocente = async (correo, password) => {
     try {
-        const response = await axios.post(url);
-        return response.data.success;  // Retorna true si el captcha es válido
-    } catch (error) {
-        console.error("Error verificando reCAPTCHA", error);
-        return false;
-    }
-}
-
-const obtenerDocente = async (correo, password, captchaToken) => {
-    try {
-        // Verificar el token de reCAPTCHA
-        const captchaValid = await verifyCaptcha(captchaToken);
-        if (!captchaValid) {
-            throw new Error('Verificación de reCAPTCHA fallida.');
-        }
-
         const query = 'SELECT cod_docente, password_docente FROM Docente WHERE correo = $1';
         const result = await db.pool.query(query, [correo]);
 
@@ -45,30 +23,23 @@ const obtenerDocente = async (correo, password, captchaToken) => {
         if (!isMatch) {
             throw new Error('Credenciales incorrectas.');
         }
-        
         const token = generarToken({ cod_docente: docente.cod_docente, role: 'docente' });
 
         // Retornar el cod_docente u otros datos necesarios
-        return { token };
+        return {token};
     } catch (err) {
         console.error('Error al autenticar docente', err);
         throw err;
     }
 };
 
-const obtenerEstudiante = async (codigoSis, password, correo, captchaToken) => {
+const obtenerEstudiante = async (codigoSis, password, correo) => {
     try {
-        // Verificar el token de reCAPTCHA
-        const captchaValid = await verifyCaptcha(captchaToken);
-        if (!captchaValid) {
-            throw new Error('Verificación de reCAPTCHA fallida.');
-        }
-
         const query = 'SELECT codigo_sis, password_estudiante FROM Estudiante WHERE codigo_sis = $1 AND correo_estudiante = $2';
         const result = await db.pool.query(query, [codigoSis, correo]);
 
         if (result.rows.length === 0) {
-            throw new Error('Credenciales incorrectas.');
+            throw new Error('Credenciales incorrectas.', err);
         }
 
         const estudiante = result.rows[0];
@@ -77,12 +48,11 @@ const obtenerEstudiante = async (codigoSis, password, correo, captchaToken) => {
         const isMatch = await bcrypt.compare(password, estudiante.password_estudiante);
 
         if (!isMatch) {
-            throw new Error('Credenciales incorrectas.');
+            throw new Error('Credenciales incorrectas.', err);
         }
-
         const token = generarToken({ codigoSis: estudiante.codigo_sis, role: 'estudiante' });
 
-        return { token };
+        return {token};
     } catch (err) {
         console.error('Error al autenticar estudiante', err);
         throw err;
@@ -93,3 +63,4 @@ module.exports = {
     obtenerDocente,
     obtenerEstudiante,
 };
+

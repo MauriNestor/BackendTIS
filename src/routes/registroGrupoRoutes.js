5d98a61e84048a3badff65bc223ee5544da5f37f
MichelleBarriga <barriga.rios.michelle1@gmail.com>
2024-10-30 20:56:52 -0400

    validar estudiante de grupo

--- src/routes/registroGrupoRoutes.js
+++ src/routes/registroGrupoRoutes.js
@@ -2,12 +2,13 @@ const express = require("express");
 const router = express.Router();
 const registroGrupoController = require("../controllers/registroGrupoController");
 const verificarToken = require("../middlewares/verificarToken");
+const verificarGrupoEstudiante = require('../middlewares/verificarGrupoEstudiante');
 
 // Ruta para registrar un grupo con el logotipo en base64
 router.post("/registrarGrupo", verificarToken, registroGrupoController.registrarGrupo);
 router.get("/:codigoClase", verificarToken, registroGrupoController.getAllGruposEmpresa);
 router.get("/:codigoClase/estudiantes-sin-grupo", verificarToken, registroGrupoController.getEstudiantesSinGruposEmpresa);
-router.get("/grupo/:codigoGrupo",verificarToken, registroGrupoController.getGrupoEmpresa);
+router.get("/grupo/:codigoGrupo",verificarToken, verificarGrupoEstudiante, registroGrupoController.getGrupoEmpresa);
 
 // Ruta para obtener los estudiantes de un grupo
 router.get('/:codGrupo/estudiantes', verificarToken, registroGrupoController.getEstudiantesDeGrupo);
